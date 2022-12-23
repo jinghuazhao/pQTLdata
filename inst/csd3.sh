1613c78c40cf65cef360dc7281d324a60e2615e0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-12-23 21:47:04 +0000

    inst/

--- inst/csd3.sh
+++ inst/csd3.sh
@@ -5,7 +5,7 @@ module load gcc/6 texlive
 Rscript -e 'setwd("~/pQTLdata");devtools::document()'
 
 export name=pQTLdata
-export version=0.0.1
+export version=0.1
 R CMD build --resave-data --compact-vignettes=both ${name}
 R CMD INSTALL ${name}_${version}.tar.gz
 
