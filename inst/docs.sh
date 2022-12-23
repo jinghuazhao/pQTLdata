8b482b5a5d1bda0dbae6d8e1ff1cd0e76071fc0f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-12-23 10:56:55 +0000

    inst/

---
+++ inst/docs.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+function create_package()
+{
+  Rscript -e 'library(usethis);create_package("pQTLdata");use_mit_license()'
+}
+
+function create_github_repository()
+{
+  export user=jinghuazhao
+  export token=$(awk "NR==2" ~/doc/access)
+  export API=https://api.github.com
+  export header="Accept: application/vnd.github.v3+json"
+  export name=pQTLdata
+
+# 1. create the repository
+  curl -X POST -u $user:$token -H "$header" -d '{"name": "'"$name"'"}' $API/user/repos
+
+# 2. list the repository
+  curl -H "$header" $API/repos/$user/$name
+
+# 3. delete the repository
+# curl -X DELETE -u $user:$token -H "$header" $API/repos/$user/$name
+  git config --global user.email "jinghuazhao@hotmail.com"
+  git config --global user.name "jinghuazhao@github.com"
+  git config --global url."https://jinghuazhao@github.com".insteadOf "https://github.com"
+}
+
+Rscript -e 'devtools::document()'
+
+for f in .gitignore .Rbuildignore .Rinstignore \
+         data/ DESCRIPTION INDEX inst/ LICENSE LICENSE.md man/ \
+         NAMESPACE NEWS.md R/ README.*
+do
+  echo adding ${f}
+  git add ${f}
+  git commit -m "${f}"
+done
+
+git add -f docs
+git commit -m "docs"
+git push
+du -h --exclude .git --exclude docs
