18911d375b08fedd95ed613fa210cdbd99b398ad
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-01-18 17:30:24 +0000

    inst/

--- inst/scripts/inf1.R
+++ inst/scripts/inf1.R
@@ -5,6 +5,12 @@ v <- c("ensembl_gene_id","chromosome_name","start_position","end_position","hgnc
 FGF5_CD6 <- unique(subset(biomaRt[v],hgnc_symbol%in%c("FGF5","CD6")))
 FGF5_CD6["uniprotswissprot"] <- with(subset(inf1,gene%in%c("FGF5","CD6")),uniprot)
 s <- unique(subset(biomaRt[v],uniprotswissprot%in%inf1$uniprot))
-inf1 <- merge(inf1,rbind(s,FGF5_CD6)[c("ensembl_gene_id","uniprotswissprot")],by.x="uniprot",by.y="uniprotswissprot")
-inf1[with(inf1,uniprot)%in%c("Q8NF90","Q8WWJ7"),"uniprot"] <- c("P12034","P30203")
+t <- merge(inf1,rbind(s,FGF5_CD6)[c("ensembl_gene_id","uniprotswissprot")],by.x="uniprot",by.y="uniprotswissprot")
+t[with(t,uniprot)%in%c("Q8NF90","Q8WWJ7"),"uniprot"] <- c("P12034","P30203")
+
+# The following reflects recent changes at www.uniprot.org
+g <- c("CCL2", "CCL7", "CCL8", "CCL11", "CCL13", "CXCL6")
+p <- c("MCP.1", "MCP.3", "MCP.2", "CCL11", "MCP.4", "CXCL6")
+pg <- data.frame(prot=p,alt_name=g)
+inf1 <- merge(t,pg,by="prot",all.x=TRUE)
 save(inf1,file="inf1.rda",compress='xz')
