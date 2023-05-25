5e73bd0e016fcf3bbe0fe16b5fd273d87a845d5e
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-05-25 11:02:38 +0100

    inst/

--- inst/scripts/inf1.R
+++ inst/scripts/inf1.R
@@ -13,4 +13,19 @@ g <- c("CCL2", "CCL7", "CCL8", "CCL11", "CCL13", "CXCL6")
 p <- c("MCP.1", "MCP.3", "MCP.2", "CCL11", "MCP.4", "CXCL6")
 pg <- data.frame(prot=p,alt_name=g)
 inf1 <- merge(t,pg,by="prot",all.x=TRUE)
+# The following reflects recent changes at https://www.ensembl.org/
+
+sevens <- "
+ENSG00000131142 CCL25 19 8052318 8062660
+ENSG00000125735 TNFSF14 19 6661253 6670588
+ENSG00000275302 CCL4 17 36103827 36105621
+ENSG00000274736 CCL23 17 36013056 36017972
+ENSG00000013725 CD6 11 60971680 61020377
+ENSG00000138675 FGF5 4 80266639 80336680
+ENSG00000277632 CCL3 17 36088256 36090169
+"
+updates <- as.data.frame(scan(file=textConnection(sevens),what=list("","",0,0,0)))
+names(updates) <- c("ensGene","gene","chromosome","start38","end38")
+inf1 <- merge(inf1,updates,by="gene",all.x=TRUE)
+
 save(inf1,file="inf1.rda",compress='xz')
