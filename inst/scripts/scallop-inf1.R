db71fea8f3d7bc5d905d06a2270c3ad874f42ce1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-08-10 20:40:34 +0100

    inst/

--- inst/scripts/scallop-inf1.R
+++ inst/scripts/scallop-inf1.R
@@ -1,10 +1,5 @@
-# INTERVAL SomaLogic results
-dir <- 'https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-018-0175-2/MediaObjects/'
-file <- '41586_2018_175_MOESM4_ESM.xlsx'
-
 # SCALLOP/INF results
-dir <- "https://jhz22.user.srcf.net/SCALLOP-INF/"
-file <- "Supplementary-Tables.xlsx"
-xlsx <- paste0(dir,file)
+
+xlsx <- 'https://static-content.springer.com/esm/art%3A10.1038%2Fs41590-023-01588-w/MediaObjects/41590_2023_1588_MOESM5_ESM.xlsx'
 scallop_inf1 <- openxlsx::read.xlsx(xlsx, sheet=5, colNames=TRUE, skipEmptyRows=TRUE, startRow=2)
 save(scallop_inf1,file='scallop_inf1.rda',compress='xz')
