93364688e166b430925c9e16c84519bda4066e12
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-12-10 17:47:42 +0000

    R/

--- R/utils.R
+++ R/utils.R
@@ -26,13 +26,18 @@
 #' @keywords datasets internal
 #' @format A data frame with 987 rows and 7 variables:
 #' \describe{
+#'   \item{\code{Gene}}{HGNC symbol}
 #'   \item{\code{Protein}}{Protein name as in UniProt}
 #'   \item{\code{Accession}}{UniProt id}
-#'   \item{\code{Gene}}{HGNC symbol}
 #'   \item{\code{Protein.Description}}{Detailed information on protein}
 #'   \item{\code{GO.Cellular.Component}}{GO Ceullular component}
 #'   \item{\code{GO.Function}}{GO function}
 #'   \item{\code{GO.Process}}{GO process}
+#'   \item{\code{ensGenes}}{Ensembl genes}
+#'   \item{\code{starts}}{start positions}
+#'   \item{\code{ends}}{end positions}
+#'   \item{\code{start}}{minimum start}
+#'   \item{\code{end}}{maximum end}
 #' }
 #' @details See the Caprion repository involving its use. Efforts have been put to check against hg19Tables in this package though a modified version is not generated except the scripts (namely, scripts/caprion.R).
 
