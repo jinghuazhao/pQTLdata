3b537f3ffccb0a96aaad825ffece813f793d21b5
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-02-19 23:17:22 +0000

    inst/

--- inst/scripts/Olink_Explore_HT.sh
+++ inst/scripts/Olink_Explore_HT.sh
@@ -3,5 +3,8 @@
 R --no-save -q <<END
   f <- file.path("..","Olink","olink-explore-ht-assay-list-2023-07-12.xlsx")
   Olink_Explore_HT <- openxlsx::read.xlsx(f, sheet=1, startRow = 5, colNames=TRUE, skipEmptyRows=TRUE)
+  f <- file.path("..","Olink","olink-explore-ht-assay-list.xlsx")
+  Olink_Explore_HT <- openxlsx::read.xlsx(f, sheet=1, startRow = 6, colNames=TRUE, skipEmptyRows=TRUE)
+  names(Olink_Explore_HT)[1] <- "Olink.ID"
   save(Olink_Explore_HT,file='../../data/Olink_Explore_HT.rda',compress='xz')
 END
